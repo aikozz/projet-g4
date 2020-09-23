2ee53b9020de48c5cbc27b7a5a6fbd9e1732eb97
nathan <pellissiere.nathan.jrd@gmail.com>
2020-09-23 12:10:49 +0200

    test

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-<?php
+<?php 
 session_start();
-require_once("lib/fonctions.php");
-include("modele/class.pdoF4Speak.php");
-include("vues/v_entete.php") ;
 
-if(!isset($_REQUEST['uc']))
-     $uc = 'accueil';
-else
-	$uc = $_REQUEST['uc'];
+//hypothèse 2 paramètres d'entrée, controle et action, avec l'url de index.php
+// exmple : index.php?controle=c1&action=a12
 
-$pdo = PdoF4Speak::getPdoF4Speak();	
+if (isset($_GET['controle']) & isset($_GET['action'])) {
+ 	$controle = $_GET['controle'];
+	$action= $_GET['action'];
+	}
+else { //absence de paramètres : prévoir des valeurs par défaut
+	$controle = "utilisateur";
+	$action= "ident";
+	}
+	
+//inclure le fichier php de contrôle 
+//et lancer la fonction-action issue de ce fichier.	
 
-switch($uc)
-{
-	case 'accueil':
-		{include("vues/v_accueil.php");break;}
-}
-include("vues/v_pied.php") ;
+	require ('./controle/' . $controle . '.php');   
+	$action (); 
+	echo "test";
+
 ?>
-
