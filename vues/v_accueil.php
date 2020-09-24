0c6e0bc4a85615516b323dff5e9d4338deafa460
nathan <pellissiere.nathan.jrd@gmail.com>
2020-09-24 17:15:06 +0200

    update header 1

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -1,14 +1,18 @@
-<div id="accueil">
-
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-
-PROJET 1 G4
-
-
-</div>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <img src="img/G4.png" class="pl-50% col-12">
+    </div>
+</body>
+</html>
--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,10 +1,58 @@
-<!DOCTYPE HTML >
-<!-- TITRE ET MENUS -->
+<!DOCTYPE html>
 <html lang="fr">
-    <head>
-            <title>F4Speak</title>
-            <meta http-equiv="Content-Language" content="fr">
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-            <link href="styles/cssGeneral.css" rel="stylesheet" type="text/css">
-    </head>
-<body >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/head.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid fondhaut">
+        <img src="img/G4.png" class=" col-12 logo">
+    </div>
+  
+              
+              
+   <ul class=" list-inline navv">
+
+            
+          
+                <li class="list-inline-item ">
+                    <a class="nav-link" href="#"> Accueil </a> 
+                </li>
+                <li class="list-inline-item ">
+                    <a class="nav-link" href="#"> Accueil </a> 
+                </li>
+                
+                <form class="navbar-form ">
+                
+                    <div class="form-group">
+                    <li class="list-inline-item float-right">
+
+                   
+<button type="submit" class="btn btn-sm btn-default">OK</button>
+ </li><li class="list-inline-item float-right">
+<input type="text" class="form-control" placeholder="Rechercher">
+
+</div>
+
+</form>
+                </li>
+                <li class="list-inline-item float-right ">
+                    <a class="nav-link" href="#"> Accueil </a> 
+                </li>
+              
+                </ul>
+                
+                     
+              
+      
+            
+      
+</body>
+</html>
