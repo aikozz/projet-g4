cc3ddc32a4dc93cae57f6f10e59d75ed289c80e2
prothtana <p.rothtana.sio@gmail.com>
2020-09-24 17:49:41 +0200

    page connexion

---
+++ vues/v_connexion.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/cssConnexion.css" >
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>F4SPEAK</title>
+</head>
+<body>
+    <div class="col-4">
+</div>
+    <div class="container-fluid col-4 mt-5">
+        <form>
+            <div class="form-group">
+                <label for="username">Identifiant : </label>
+                <input type="text" class="form-control" placeholder="Entrez votre pseudo..." name="id" required>
+            </div>
+            <div class="form-group">
+                <label for="mdp">Mot de passe : </label>
+                <input type="password" class="form-control" placeholder="Entrez votre mot de passe..." name="mdp" required>
+            </div>
+            <input type="submit" class="btn" name="connection" value="Se connecter">
+        </form>
+    </div>
+    <div class="col-4">
+</div>
